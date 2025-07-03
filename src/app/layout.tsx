d582ae4bc2ce2ed5f49c7d872b054c84e3593f16
Leclamar <macellacalista@gmail.com>
2025-07-03 20:36:39 +0700

    1st

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  display: 'swap', 
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
